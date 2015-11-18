7466706d6343b0b097ef44faa99f093092dbdeee
Sunny Wijeratne <sunny.wijeratne@gmail.com>
2015-11-18 15:09:51 +1000

    "Create List Item" console app
    Creates a list item based on parameters in app.config. Can be published
    as an Azure Web Job.

---
+++ CreateListItemWebJob/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+
+namespace CreateListItemWebJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var now = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss");
+
+            var username = ConfigurationManager.AppSettings["username"];
+            var password = ConfigurationManager.AppSettings["password"];
+            var siteUrl = ConfigurationManager.AppSettings["siteUrl"];
+            var listName = ConfigurationManager.AppSettings["listName"];
+
+            var securePassword = new SecureString();
+            foreach (var c in password.ToCharArray()) securePassword.AppendChar(c);
+
+            var clientContext = new ClientContext(siteUrl);
+            clientContext.Credentials = new SharePointOnlineCredentials(username, securePassword);
+
+            var scheduleList = clientContext.Web.Lists.GetByTitle(listName);
+            clientContext.Load(scheduleList);
+
+            var listItemCreationInfo = new ListItemCreationInformation();
+            var listItem = scheduleList.AddItem(listItemCreationInfo);
+            
+            listItem["Title"] = "WebJob " + now;
+
+            listItem.Update();
+            clientContext.ExecuteQuery();
+
+        }
+    }
+}
